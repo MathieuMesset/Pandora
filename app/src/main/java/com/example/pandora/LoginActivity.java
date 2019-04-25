3397fe204c0fc7868db23e878dda1c363fa48780
Mathieu Messet <messet@et.esiea.fr>
2019-04-25 17:12:16 +0200

    Added transition between user selection and login

--- app/src/main/java/com/example/pandora/LoginActivity.java
+++ app/src/main/java/com/example/pandora/LoginActivity.java
@@ -5,16 +5,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+
+
+
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String USERNAME = "Username";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        displayInfo();
 
     }
+
+
+    public void displayInfo(){
+
+        TextView usernameView = (TextView)findViewById(R.id.Username);
+
+        String username = getIntent().getStringExtra("USERNAME");
+
+        usernameView.setText(username);
+
+    }
+
 
     public void Check_Password(View view) {
 
